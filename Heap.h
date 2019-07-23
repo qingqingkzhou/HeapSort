4e9d732eef16a211fe6d7a926973d72b04a1407c
Qingqing Zhou <qingqing.k.zhou@gmail.com>
2019-07-23 16:04:47 -0400

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -18,20 +18,34 @@ namespace gm
         explicit Heap(const size_t size);
         virtual ~Heap() = default;
 
-        template<class Type>
-        void Sort();
-
-        void TestSort(const size_t retries);
-
-        /* function to print array of size n */
         void Print() const;
-
-        void Resize(const size_t size);
         void Generate();
 
+        void Resize(const size_t size);
+
+        template<class SortType>
+        void Sort();
+
+        template<class SortType>
+        void TestSort(const size_t n_trials)
+        {
+            long long sum{ 0 };
+            for (int i = 0; i < n_trials; i++)
+            {
+                // Randomly generate values in the vector
+                Generate();
+
+                // Find the duration for Sort()
+                sum += TimedSort([this] { this->Sort<SortType>(); });
+            }
+            // Caldulate the average performance
+            std::cout << "Time taken [Average]: " <<
+                sum / n_trials << " microseconds\n\n";
+        }
+
     private:
-        long long TimedSort(FuncType func);
         void Heapify(const size_t size, const size_t root);
+        long long TimedSort(FuncType func);
 
         Container m_container;
     };
